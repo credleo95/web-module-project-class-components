59753874849a54efc3406cd6f4648924b3b66ff9
Omarius Credle <omariuscredle@omariuss-air.home>
2021-05-05 21:51:28 -0400

    created state for todos, adding todos, clear todos, and toggle completed todos.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
 import React from 'react';
 
+
+const todos =[{
+      task: '',
+      id: null,
+      completed: false
+}]
 class App extends React.Component {
+  constructor(){
+    super(); 
+    this.state = {
+      todo: todos
+    }
+  }
+  addTodo = newTodo => {
+    this.setState({
+      todo: [...this.state.todos, {
+        task: newTodo,
+        id: Date.now(),
+        completed: false
+      }]
+    })
+  }
+  toggleCompleted = todoId => {
+    this.setState({
+      todo: this.state.todos.map(todo => {
+        if (todo.id === todoId){
+          return {
+            ...todo,
+            completed:!todo.completed
+          }
+        }
+        return todo; 
+      })
+    })
+  }
+
+  clearPurchased = () => {
+    this.setState({
+      todo: this.state.todos.filter(tasks => !tasks.completed)
+    })
+  }
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
